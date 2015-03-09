567eef3b7c8b0cb4f5c0ac82e6c3aa4187f4ac65
fushang318 <fushang318@gmail.com>
2015-03-09 11:31:13 +0800

    增加导航条

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -27,16 +27,4 @@ class SessionController < ApplicationController
     redirect_to "/"
   end
 
-  def current_sign_in=(sign_user)
-    cookies.permanent.signed[:current_sign_in] = sign_user.email
-  end
-
-  def current_sign_in
-    SignUser.find_by_email(cookies.signed[:current_sign_in])
-  end
-
-  def delete_current_sign_in
-    cookies.permanent.signed[:current_sign_in] = nil
-  end
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_sign_in=(sign_user)
+    cookies.permanent.signed[:current_sign_in] = sign_user.email
+  end
+
+  def current_sign_in
+    SignUser.find_by_email(cookies.signed[:current_sign_in])
+  end
+
+  def delete_current_sign_in
+    cookies.permanent.signed[:current_sign_in] = nil
+  end
 end
